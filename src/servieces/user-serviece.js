2fed69011b29757224c879305763512f99bff056
Rushabh1105 <rushabh.sakharkar1998@gmail.com>
2022-12-29 18:40:36 +0530

    added jwt verification strategy

--- src/servieces/user-serviece.js
+++ src/servieces/user-serviece.js
@@ -1,4 +1,7 @@
+const jwt = require("jsonwebtoken");
+
 const UserRepository = require("../repository/user-repository");
+const { JWT_KEY } = require("../config/serverConfig");
 
 class UserServiece{
     constructor(){
@@ -21,6 +24,26 @@ class UserServiece{
             return user;
         } catch (error) {
             console.log("Error at user serviece layer");
+            throw {error};
+        }
+    }
+
+    async createToken(user){
+        try {
+           const result = jwt.sign(user, JWT_KEY, {expiresIn : '1h'});
+           return result; 
+        } catch (error) {
+            console.log("something went wrong in token creation");
+            throw {error};
+        }
+    }
+
+    async varifyToken(){
+        try {
+            const response = jwt.verify(token, JWT_KEY);
+            return response;
+        } catch (error) {
+            console.log("something went wrong in token validation", error);
             throw {error};
         }
     }
